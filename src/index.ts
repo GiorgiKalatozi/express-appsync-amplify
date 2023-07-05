ac525ad89a8275d20c8dd76734f787f791fcd46b
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-07-05 02:27:30 -0800

    project setup is done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-import * as fs from "fs";
+import express, { Request, Response } from "express";
+
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("HELLO FROM EXPRESS TYPESCRIPT SERVER");
+});
+app.get("/about", (req: Request, res: Response) => {
+  res.send("about us hii king ragnar say my name");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
