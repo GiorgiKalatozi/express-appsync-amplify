e50a68482c078eee319b11fc1ac74ae733dcfb0a
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-07-06 00:32:04 -0800

    latest

--- src/index.ts
+++ src/index.ts
@@ -191,70 +191,208 @@
 //   console.log("Server is running on http://localhost:3000/graphql");
 // });
 
+// import express from "express";
+// import { ApolloServer, gql } from "apollo-server-express";
+// import { AWSAppSyncClient } from "aws-appsync";
+// import { DynamoDBStreams } from "aws-sdk";
+
+// // Create an instance of the Express server
+// const app = express();
+
+// // Define your GraphQL schema
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//   }
+
+//   type Subscription {
+//     onMessageAdded: Message
+//   }
+
+//   type Message {
+//     id: ID!
+//     content: String!
+//   }
+// `;
+
+// // Define the resolver functions
+// const resolvers = {
+//   Query: {
+//     hello: () => "Hello, world!",
+//   },
+//   Subscription: {
+//     onMessageAdded: {
+//       subscribe: (_: any, __: any, { appSyncClient }: any) => {
+//         const ddbStreamsClient = new DynamoDBStreams({
+//           region: "eu-central-1",
+//         }); // Replace with your AWS region
+//         const streamArn = "YOUR_DYNAMODB_STREAM_ARN"; // Replace with your DynamoDB stream ARN
+
+//         const params = {
+//           StreamArn: streamArn,
+//           ShardId: "shardId", // Replace with your DynamoDB shard ID
+//           ShardIteratorType: "LATEST",
+//         };
+
+//         const processRecords = async () => {
+//           try {
+//             const iterator = await ddbStreamsClient
+//               .getShardIterator(params)
+//               .promise();
+//             const iteratorParams = {
+//               ShardIterator: iterator.ShardIterator,
+//               Limit: 100, // Set the desired batch size
+//             };
+
+//             // eslint-disable-next-line no-constant-condition
+//             while (true) {
+//               const records = await ddbStreamsClient.getRecords().promise();
+
+//               for (const record of records.Records || []) {
+//                 if (record) {
+//                   const message = {
+//                     id: 1,
+//                     content: 2,
+//                   };
+
+//                   appSyncClient.publish({
+//                     query: gql`
+//                       subscription {
+//                         onMessageAdded {
+//                           id: ${message.id}
+//                           content: ${message.content}
+//                         }
+//                       }
+//                     `,
+//                     variables: {},
+//                   });
+//                 }
+//               }
+
+//               iteratorParams.ShardIterator = records.NextShardIterator;
+//             }
+//           } catch (error) {
+//             console.error("Error processing records:", error);
+//           }
+
+//           processRecords(); // Continuously process new records
+//         };
+
+//         processRecords(); // Start processing records
+
+//         // Return an empty async iterator as per the GraphQL subscription format
+//         return {
+//           [Symbol.asyncIterator]: () => ({
+//             next: () =>
+//               new Promise((resolve) => {
+//                 console.log(resolve);
+//               }),
+//           }),
+//         };
+//       },
+//     },
+//   },
+// };
+
+// // Configure AWS AppSync client
+// const appSyncClient = new AWSAppSyncClient({
+//   url: "https://bs6z527jgvah3oy2cx3pdjmkvy.appsync-api.eu-central-1.amazonaws.com/graphql",
+//   region: "eu-central-1",
+//   auth: {
+//     type: "AWS_IAM",
+// credentials: {
+//   accessKeyId: "AKIATJRIWQW4KCUN7EOA",
+//   secretAccessKey: "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",
+// },
+//   },
+// });
+
+// // Create Apollo Server
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: () => ({ appSyncClient }),
+// });
+
+// console.log(appSyncClient, "appSyncClient");
+
+// // Start the server and apply middleware
+// async function startServer() {
+//   await server.start();
+//   server.applyMiddleware({ app });
+// }
+
+// // Call the startServer function
+// startServer()
+//   .then(() => {
+//     // Start the server after applying middleware
+//     app.listen(3000, () => {
+//       console.log("Server is running on http://localhost:3000/graphql");
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error starting the server:", error);
+//   });
+
 import express from "express";
 import { ApolloServer, gql } from "apollo-server-express";
-import { AWSAppSyncClient } from "aws-appsync";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PubSub } from "graphql-subscriptions";
 
-// Create an instance of the Express server
 const app = express();
+const pubsub = new PubSub();
 
-// Define your GraphQL schema
 const typeDefs = gql`
+  type Message {
+    id: ID!
+    content: String!
+  }
+
   type Query {
-    hello: String
+    messages: [Message]
   }
 
   type Subscription {
-    onMessageAdded: String
+    newMessage: Message
   }
 `;
 
-// Define the resolver functions
+const messages: any = [];
 const resolvers = {
   Query: {
-    hello: () => "Hello, world!",
+    messages: () => messages,
   },
   Subscription: {
-    onMessageAdded: {
-      subscribe: (_: any, __: any, { appSyncClient }: any) => {
-        return appSyncClient.subscribe({
-          query: gql`
-            subscription {
-              onMessageAdded {
-                message
-              }
-            }
-          `,
-        });
-      },
+    newMessage: {
+      subscribe: () => pubsub.asyncIterator("NEW_MESSAGE"),
     },
   },
 };
 
-// Configure AWS AppSync client
-const appSyncClient = new AWSAppSyncClient({
-  url: "YOUR_APPSYNC_API_URL",
-  region: "YOUR_AWS_REGION",
-  auth: {
-    type: "AWS_IAM",
-    credentials: {
-      accessKeyId: "YOUR_AWS_ACCESS_KEY",
-      secretAccessKey: "YOUR_AWS_SECRET_ACCESS_KEY",
-    },
+const dynamoDBClient = new DynamoDBClient({
+  region: "eu-central-1",
+  credentials: {
+    accessKeyId: "AKIATJRIWQW4KCUN7EOA",
+    secretAccessKey: "dM/b3039l+a84GWZRnYxrZL5Wie0BmjvxnJXbQIy",
   },
 });
 
-// Create Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => ({ appSyncClient }),
+  context: { pubsub, dynamoDBClient },
 });
 
-// Apply Apollo Server middleware to the Express app
-server.applyMiddleware({ app });
+async function startApolloServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000/graphql");
+startApolloServer().then(() => {
+  const port = 4000;
+  app.listen(port, () => {
+    console.log(
+      `Server running at http://localhost:${port}${server.graphqlPath}`
+    );
+  });
 });
